3182377ae5f2c0b45e616095cefc1283c54271d3
ben balsley <benbalsley@Bens-MacBook-Air.local>
2025-11-20 11:24:51 -0500

    Set account to  and risk to 2% per trade

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ import os
 SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL", "")
 
 # Your trading account size in dollars
-ACCOUNT_EQUITY = 2000.0
+ACCOUNT_EQUITY = 1000.0
 
 # Fraction of account risked per trade (0.01 = 1%)
-RISK_PER_TRADE_PCT = 0.01
+RISK_PER_TRADE_PCT = 0.02
 
 # File that contains the list of tickers to scan
 UNIVERSE_FILE = "universe.txt"
